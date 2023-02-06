7938bc95457a18422be018eb096ad00ae548a9af
Patrick Zulian <patrick.zulian@gmail.com>
2023-02-06 16:45:31 +0100

    improved print

--- print_crs.c
+++ print_crs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "matrixio_crs.h"
 
@@ -24,6 +25,15 @@ int main(int argc, char *argv[]) {
 
         return EXIT_FAILURE;
     }
+
+    int MATRIXIO_DENSE_OUTPUT = 0;
+    MATRIXIO_READ_ENV(MATRIXIO_DENSE_OUTPUT, atoi);
+
+    int MATRIXIO_SHOW_PROCESS_RANK = 1;
+    MATRIXIO_READ_ENV(MATRIXIO_SHOW_PROCESS_RANK, atoi);
+
+    int MATRIXIO_PRINT_CSV = 0;
+    MATRIXIO_READ_ENV(MATRIXIO_PRINT_CSV, atoi);
 
     MPI_Datatype rowptr_type = MPI_INT;
     MPI_Datatype colidx_type = MPI_INT;
@@ -53,33 +63,79 @@ int main(int argc, char *argv[]) {
     CATCH_MPI_ERROR(MPI_Type_size(crs.colidx_type, &colidx_type_size));
     CATCH_MPI_ERROR(MPI_Type_size(crs.values_type, &values_type_size));
 
-    for (int r = 0; r < size; ++r) {
-        if (r == rank) {
-            printf("[%d]\n", rank);
-            for (ptrdiff_t i = 0; i < crs.lrows; ++i) {
-                ptrdiff_t begin = to_ptrdiff_t(rowptr_type, &crs.rowptr[i * rowptr_type_size]) - crs.start;
-                ptrdiff_t end = to_ptrdiff_t(rowptr_type, &crs.rowptr[(i + 1) * rowptr_type_size]) - crs.start;
 
-                printf("row %ld)\n", i);
-                printf("cols: ");
+    if (MATRIXIO_DENSE_OUTPUT) {
+        ptrdiff_t nnz = to_ptrdiff_t(rowptr_type, &crs.rowptr[crs.lrows * rowptr_type_size]);
+        ptrdiff_t maxcol = 0;
 
-                for (ptrdiff_t k = begin; k < end; k++) {
-                    printf("%ld ", to_ptrdiff_t(colidx_type, &crs.colidx[k * colidx_type_size]));
-                }
-
-                printf("\n");
-
-                printf("vals: ");
-                for (ptrdiff_t k = begin; k < end; k++) {
-                    printf("%g ", to_double(values_type, &crs.values[k * values_type_size]));
-                }
-
-                printf("\n");
-            }
-            printf("\n");
+        for (ptrdiff_t k = 0; k < nnz; k++) {
+            ptrdiff_t col = to_ptrdiff_t(colidx_type, &crs.colidx[k * colidx_type_size]);
+            maxcol = MAX(col, maxcol);
         }
 
-        MPI_Barrier(comm);
+        maxcol += 1;
+        double *buff = malloc(sizeof(double) * maxcol);
+
+        for (int r = 0; r < size; ++r) {
+            if (r == rank) {
+                if (MATRIXIO_SHOW_PROCESS_RANK) {
+                    printf("[%d]\n", rank);
+                }
+
+                for (ptrdiff_t i = 0; i < crs.lrows; ++i) {
+                    memset(buff, 0, sizeof(double) * maxcol);
+                    ptrdiff_t begin = to_ptrdiff_t(rowptr_type, &crs.rowptr[i * rowptr_type_size]) - crs.start;
+                    ptrdiff_t end = to_ptrdiff_t(rowptr_type, &crs.rowptr[(i + 1) * rowptr_type_size]) - crs.start;
+
+                    for (ptrdiff_t k = begin; k < end; k++) {
+                        ptrdiff_t col = to_ptrdiff_t(colidx_type, &crs.colidx[k * colidx_type_size]);
+                        double val = to_double(values_type, &crs.values[k * values_type_size]);
+                        buff[col] = val;
+                    }
+
+                    for (ptrdiff_t c = 0; c < maxcol; c++) {
+                        if(c + 1 < maxcol) {
+                            printf("%g%s", buff[c], MATRIXIO_PRINT_CSV? "," : " ");
+                        } else {
+                            printf("%g", buff[c]);
+                        }
+                    }
+
+                    printf("\n");
+                }
+            }
+        }
+    } else {
+        for (int r = 0; r < size; ++r) {
+            if (r == rank) {
+                if (MATRIXIO_SHOW_PROCESS_RANK) {
+                    printf("[%d]\n", rank);
+                }
+                for (ptrdiff_t i = 0; i < crs.lrows; ++i) {
+                    ptrdiff_t begin = to_ptrdiff_t(rowptr_type, &crs.rowptr[i * rowptr_type_size]) - crs.start;
+                    ptrdiff_t end = to_ptrdiff_t(rowptr_type, &crs.rowptr[(i + 1) * rowptr_type_size]) - crs.start;
+
+                    printf("row %ld)\n", i);
+                    printf("cols: ");
+
+                    for (ptrdiff_t k = begin; k < end; k++) {
+                        printf("%ld ", to_ptrdiff_t(colidx_type, &crs.colidx[k * colidx_type_size]));
+                    }
+
+                    printf("\n");
+
+                    printf("vals: ");
+                    for (ptrdiff_t k = begin; k < end; k++) {
+                        printf("%g ", to_double(values_type, &crs.values[k * values_type_size]));
+                    }
+
+                    printf("\n");
+                }
+                printf("\n");
+            }
+
+            MPI_Barrier(comm);
+        }
     }
 
     crs_free(&crs);
--- utils.h
+++ utils.h
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <mpi.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 #define CATCH_MPI_ERROR(err)      \
     {                             \
@@ -12,7 +13,16 @@
         }                         \
     }
 
+#define MATRIXIO_READ_ENV(name, conversion) \
+    do {                               \
+        char *var = getenv(#name);     \
+        if (var) {                     \
+            name = conversion(var);    \
+        }                              \
+    } while (0)
+
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 ptrdiff_t to_ptrdiff_t(MPI_Datatype type, const char *data);
 double to_double(MPI_Datatype type, const char *data);
